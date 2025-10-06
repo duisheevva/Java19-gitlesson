91c4d10739a1d8a3a2f814ed58ed7efbc257bcec
duisheevvva <duisheeva.aizat@icloud.com>
2025-10-06 14:08:48 +0600

    added student class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        // Step1 - Open project
+        // Step2 - add collaborators
+        // Step3 - clone project
+        // Step4 - create branch
         System.out.println("Hello World");
     }
 }
